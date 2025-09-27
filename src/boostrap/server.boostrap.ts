03ae50973fa065ee608e789a173bcfacd0b79bdb
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-09-27 00:48:20 -0500

    añadir la funcionalidad de promise y las maneras que va a tener el servidor de responder

--- src/boostrap/server.boostrap.ts
+++ src/boostrap/server.boostrap.ts
@@ -8,12 +8,17 @@ export class ServerBoostrap {
         this.app = app;
     }
 
-    init() {
-        const server = http.createServer(this.app);
-        const port = process.env.PORT || 4000;
-
-        server.listen(port, () => {
-            console.log(`Server is running on port http://localhost:${port}`);
+    init(): Promise<Boolean> {
+        return new Promise((resolve, reject) => {
+            const server = http.createServer(this.app);
+            const PORT = process.env.PORT || 4000;
+            server.listen(PORT).on("listening", () => {
+                console.log(`Server running on the directory http://localhost:${PORT}`);
+                resolve(true);
+            })
+            .on("error", (error) =>{
+                console.log(`Error starting the server: ${error}`);
+            })
         });
     }
 }
